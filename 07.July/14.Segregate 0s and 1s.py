505d5b9a8dede3d78a4cc91d2f0d21b7294b1b3a
Deepak Sharma <deepak5112003@gamil.com>
2024-07-14 13:56:54 +0530

    Added POTD Segregate 0s and 1s

---
+++ 07.July/14.Segregate 0s and 1s.py
@@ -0,0 +1,48 @@
+# User function Template for python3
+
+class Solution:
+    def segregate0and1(self, arr):
+        # code here
+        start = 0
+        end = len(arr)-1
+        while start < end:
+            if arr[start] == 0:
+                start += 1
+            elif arr[end] == 1:
+                end -= 1
+            else:
+                arr[start], arr[end] = arr[end], arr[start]
+                start += 1
+                end -= 1
+        return arr
+
+
+# {
+ # Driver Code Starts
+class IntArray:
+
+    def __init__(self) -> None:
+        pass
+
+    def Input(self, n):
+        arr = [int(i) for i in input().strip().split()]  # array input
+        return arr
+
+    def Print(self, arr):
+        for i in arr:
+            print(i, end=" ")
+        print()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+
+        arr = list(map(int, input().strip().split()))
+
+        obj = Solution()
+        obj.segregate0and1(arr)
+
+        print(*arr)
+
+# } Driver Code Ends
